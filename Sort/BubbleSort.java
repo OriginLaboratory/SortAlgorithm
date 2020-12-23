cbd89e15f9197d645877101ef6e0fcf415bf2dfa
OriginLaboratory <76254401+OriginLaboratory@users.noreply.github.com>
2020-12-23 20:28:29 +0800

    add annotations

--- Sort/BubbleSort.java
+++ Sort/BubbleSort.java
@@ -4,9 +4,9 @@ public class BubbleSort{
 		 int i=0;
 		 int len=array.length;
 		 double temp;
-		 for(;i<len;i++)
+		 for(;i<len-1;i++)//冒泡排序共需要排len-1次，因为最后一个自动排好
 			 for(int j=0;j<len-1-i;j++) {
-				 if(array[j]>array[j+1])
+				 if(array[j]>array[j+1])//前一个与后一个比较，如果前一个大于后一个则交换，像泡泡一样每次把排序列里最大的放到最右端
 				 {				
 					 temp=array[j];
 					 array[j]=array[j+1];
